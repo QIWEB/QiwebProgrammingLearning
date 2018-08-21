eff47bcb30e79bdcb6bf280e0e467b8d4c648e44
qiweb2018 <zhangqi@dbgo.cn>
2018-08-21 15:44:10 +0800

    java代码

---
+++ javastuty/src/main/java/com/qiweb/JavastutyApplication.java
@@ -0,0 +1,12 @@
+package com.qiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavastutyApplication.class, args);
+	}
+}
